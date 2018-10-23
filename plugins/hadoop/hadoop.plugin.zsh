8f1e28888d28b606a4ad57d8e1e69e1a74b94836
Wan Wenli <swwl1992@gmail.com>
2018-10-23 10:38:53 +0800

    update hadoop-plugin and reorder instructions in installation script

--- plugins/hadoop/hadoop.plugin.zsh
+++ plugins/hadoop/hadoop.plugin.zsh
@@ -1,7 +1,7 @@
 export SPARK_DEFAULT_CONF="--conf spark.executor.memory=2g --conf spark.executor.cores=2 --conf spark.executor.instances=5 --conf spark.dynamicAllocation.enabled=false"
 
 alias ipyspark="PYSPARK_DRIVER_PYTHON=ipython pyspark $SPARK_DEFAULT_CONF"
-alias jupyspark="PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser' pyspark $SPARK_DEFAULT_CONF"
+alias jupyspark="PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=0.0.0.0' pyspark $SPARK_DEFAULT_CONF"
 alias hls="hadoop fs -ls"
 alias hlsh="hadoop fs -ls -h"
 alias hcp="hadoop fs -cp"
--- tools/install.sh
+++ tools/install.sh
@@ -78,6 +78,12 @@ main() {
   " ~/.zshrc > ~/.zshrc-omztemp
   mv -f ~/.zshrc-omztemp ~/.zshrc
 
+  echo 'Installing plugin: zsh-syntax-highlighting'
+  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+  echo 'Installing plugin: zsh-autosuggestions'
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
   # If this user's login shell is not already "zsh", attempt to switch.
   TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
   if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
@@ -92,12 +98,6 @@ main() {
     fi
   fi
 
-  echo 'Installing plugin: zsh-syntax-highlighting'
-  git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
-  echo 'Installing plugin: zsh-autosuggestions'
-  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
   printf "${GREEN}"
   echo '         __                                     __   '
   echo '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
@@ -107,11 +107,8 @@ main() {
   echo '                        /____/                       ....is now installed!'
   echo ''
   echo 'This is a forked version produced by Wan Wenli (swwl1992)'
+  echo ''
   echo 'Please look over the ~/.zshrc file to select plugins, themes, and options.'
-  echo ''
-  echo 'p.s. Follow us at https://twitter.com/ohmyzsh.'
-  echo ''
-  echo 'p.p.s. Get stickers and t-shirts at https://shop.planetargon.com.'
   echo ''
   printf "${NORMAL}"
   env zsh -l
